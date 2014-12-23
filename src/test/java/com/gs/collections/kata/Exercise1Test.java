54cfb35043b977016401bbc237abe5602ca6a9db
Connor Holm <cmtholm@umich.edu>
2014-12-22 19:47:25 -0500

    Excercise 1 complete

--- src/test/java/com/gs/collections/kata/Exercise1Test.java
+++ src/test/java/com/gs/collections/kata/Exercise1Test.java
@@ -17,6 +17,7 @@
 package com.gs.collections.kata;
 
 import com.gs.collections.api.block.function.Function;
+import com.gs.collections.api.block.predicate.Predicate;
 import com.gs.collections.api.list.MutableList;
 import com.gs.collections.impl.list.mutable.FastList;
 import com.gs.collections.impl.test.Verify;
@@ -41,7 +42,7 @@ public class Exercise1Test extends CompanyDomainForKata
          * Get the name of each of the company's customers.
          */
         MutableList<Customer> customers = this.company.getCustomers();
-        MutableList<String> customerNames = null;
+        MutableList<String> customerNames = customers.collect(nameFunction);
 
         MutableList<String> expectedNames = FastList.newListWith("Fred", "Mary", "Bill");
         Assert.assertEquals(expectedNames, customerNames);
@@ -56,7 +57,11 @@ public class Exercise1Test extends CompanyDomainForKata
          * ctrl+i in IntelliJ. Eclipse's ctrl+1 is auto-fix and works to implement interfaces.
          */
         MutableList<Customer> customers = this.company.getCustomers();
-        MutableList<String> customerCities = null;
+        MutableList<String> customerCities = customers.collect( new Function<Customer, String>() {
+        	public String valueOf(Customer customer) {
+        		return customer.getCity();
+        	}
+        });
 
         MutableList<String> expectedCities = FastList.newListWith("London", "Liphook", "London");
         Assert.assertEquals(expectedCities, customerCities);
@@ -68,8 +73,13 @@ public class Exercise1Test extends CompanyDomainForKata
         /**
          * Which customers come from London? Get a collection of those which do. Use an anonymous inner class.
          */
+    	
         MutableList<Customer> customers = this.company.getCustomers();
-        MutableList<Customer> customersFromLondon = null;
+        MutableList<Customer> customersFromLondon = customers.select( new Predicate<Customer>() {
+            	public boolean accept(Customer customer) {
+            		return customer.getCity() == "London";
+            	}
+        });
         Verify.assertSize("Should be 2 London customers", 2, customersFromLondon);
     }
 }
